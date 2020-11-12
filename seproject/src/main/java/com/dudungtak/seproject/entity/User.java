2de989b4022de7182e6fc1b4bf9664d3dd312422
dormarble <gudfla0814@gmail.com>
2020-11-12 19:44:14 +0900

    connect to mysql server

---
+++ seproject/src/main/java/com/dudungtak/seproject/entity/User.java
@@ -0,0 +1,45 @@
+package com.dudungtak.seproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Accessors(chain = true)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String gender;
+
+    private String address;
+
+    private String phoneNumber;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
+}
---
+++ seproject/src/main/java/com/dudungtak/seproject/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.dudungtak.seproject.repository;
+
+import com.dudungtak.seproject.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
