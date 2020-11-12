d8da57b58d264ca3328f156fb96f855d850f540f
dormarble <gudfla0814@gmail.com>
2020-11-12 16:21:53 +0900

    setting project

---
+++ seproject/src/main/java/com/dudungtak/seproject/SeprojectApplication.java
@@ -0,0 +1,13 @@
+package com.dudungtak.seproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeprojectApplication.class, args);
+	}
+
+}
---
+++ seproject/src/test/java/com/dudungtak/seproject/SeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dudungtak.seproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
