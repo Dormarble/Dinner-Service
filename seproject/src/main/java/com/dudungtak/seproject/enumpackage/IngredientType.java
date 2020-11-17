3fc11882633d941d2ddd975073745fea47f75399
dormarble <gudfla0814@gmail.com>
2020-11-17 20:16:38 +0900

    fix enum2

--- seproject/src/main/java/com/dudungtak/seproject/enumpackage/IngredientType.java
+++ seproject/src/main/java/com/dudungtak/seproject/enumpackage/IngredientType.java
@@ -1,4 +1,14 @@
 package com.dudungtak.seproject.enumpackage;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum IngredientType {
+    VEGETABLE(0, "vegetable", "아채");
+
+    private Integer id;
+    private String title;
+    private String description;
 }
